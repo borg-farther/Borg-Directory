cef0a1746e127b1578a544354f3f42f8ebe08a0c
borg-farther <admin@borg.directory>
2026-03-28 11:33:57 +0000

    cleanup: remove debug scripts

--- check_import.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, '/root/hermes-workspace/guild-v2')
-try:
-    import borg
-    print("OK:", borg.__file__)
-except Exception as e:
-    print("FAIL:", e)
--- debug_regex.py
+++
@@ -1,8 +0,0 @@
-import re
-content = '## Guild Workflow Packs\n\nOld guild content here.\n\n## Other Section\n\nMore stuff.\n'
-pattern = r'(\n)?## Guild Workflow Packs.*?(?=\n## |\Z)'
-instructions = '\n[NEW GUILD CONTENT]'
-new_content = re.sub(pattern, instructions, content, flags=re.DOTALL)
-print('Result:', repr(new_content))
-print('Old gone?', 'Old guild content' not in new_content)
-print('More still there?', 'More stuff' in new_content)
